c6ad2ad1876b0eb7332171e3fb761a8b13bdbcea
Ling Li <tinker_code@outlook.com>
2021-02-22 20:10:59 +0800

    Jerry: Upload source codes.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# Deploy the blog & upload source codes conveniently.
+
+# Upload source codes.
+git add .
+git commit -m "Jerry: Upload source codes."
+git push origin hexo
+
+# Deploy the blog.
+hexo clean
+hexo generate
+hexo deploy
+
+# Done.
+echo "> Uploading & Deployment COMPLETED."
+
